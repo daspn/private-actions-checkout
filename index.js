011bad7b5dbbb3d590b7cf1746fffd1e3c3bcfec
Daniel Augusto Paes Nunes <daniel.nunes@zoop.com.br>
2020-06-01 16:08:47 -0300

    Adding the initial code

---
+++ index.js
@@ -0,0 +1,31 @@
+const core = require("@actions/core");
+const exec = require('@actions/exec');
+
+async function run() {
+  try {
+    const actionsList = JSON.parse(core.getInput("actions_list"));
+    const basePath = core.getInput("actions_base_path");
+
+    const regex = /^(.+)\/(.+)@(.+)$/;
+    actionsList.forEach(async (action) => {
+      const match = regex.exec(action)
+      if(match.length === 4) {
+        const owner = match[1];
+        const repo = match[2];
+        const branch = match[3];
+
+        const cloneUrl = `git@github.com:${owner}/${repo}.git`;
+        const cloneCommand = `git clone --depth=1 --single-branch --branch ${branch} ${cloneUrl} ${basePath}/${repo}`
+        console.log(cloneCommand)
+        await exec.exec(cloneCommand);
+
+      } else {
+        console.log(`The value ${action} does not follow the required format: owner/repo@branch`)
+      }
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
